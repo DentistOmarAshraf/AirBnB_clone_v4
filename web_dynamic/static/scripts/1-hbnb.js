9b99d7e92771e4c4a076735ef6f4f649985b67b8
DentistOmarAshraf <omar_ashraf@live.com>
2024-06-02 17:09:45 +0000

    [Solved] overflow amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,19 @@
-$(document).ready(function(){
-	amen = [];
-	$('li input').change(function(){
-		if ($(this).is(':checked')){
-			amen.push($(this).data('name'));
-		}else {
-			amen.pop($(this).data('name'));
-		}
-		if (amen.length === 0){
-			$('.amenities H4').html('&nbsp;')
-		}else {
-			$('.amenities H4').text(amen.join(', '))
-		}
-	})
-})
+$(document).ready(function () {
+  let amen = [];
+  $('li input').change(function () {
+    if ($(this).is(':checked')) {
+      amen.push($(this).data('name'));
+    } else {
+      amen = amen.filter(name => name !== $(this).data('name'));
+    }
+    if (amen.length === 0) {
+      $('.amenities H4').html('&nbsp;');
+    } else {
+      $('.amenities H4').text(amen.join(', '));
+    }
+  });
+  $('.amenities H4').css('height', 'max-content');
+  $('.amenities H4').css('text-overflow', 'ellipsis');
+  $('.amenities H4').css('white-space', 'nowrap');
+  $('.amenities H4').css('overflow', 'hidden');
+});
